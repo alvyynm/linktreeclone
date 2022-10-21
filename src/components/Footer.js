512556d75f1b0e33feb2196ef0d9104968d549ed
alvyynm <alvyynm@gmail.com>
2022-10-21 23:05:28 +0300

    Add JSX to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
+import slack from "../assets/slack.png";
+import github from "../assets/github.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="wrapper">
+        <img src={slack} alt="Slack logo" />
+        <img src={github} alt="GitHub logo" />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
