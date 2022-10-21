d288007c9498a2ef7346843bf39781458c7b044a
alvyynm <alvyynm@gmail.com>
2022-10-21 21:58:10 +0300

    Create Footer component and display in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Links from "./components/Links";
 import Profile from "./components/Profile";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Profile />
       <Links />
+      <Footer />
     </div>
   );
 };
