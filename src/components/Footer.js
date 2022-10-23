8c64092c5237e917c5e4b2a6a36af7be42369f91
alvyynm <alvyynm@gmail.com>
2022-10-23 14:12:30 +0300

    Style Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,18 @@
 import React from "react";
 import slack from "../assets/slack.png";
 import github from "../assets/github.png";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="wrapper">
-        <img src={slack} alt="Slack logo" />
-        <img src={github} alt="GitHub logo" />
+        <a href="#" className="social-icon">
+          <img src={slack} alt="Slack logo" />
+        </a>
+        <a href="#" className="social-icon">
+          <img src={github} alt="GitHub logo" />
+        </a>
       </div>
     </div>
   );
