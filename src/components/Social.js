a5208f4a5d592978308b3488b289d53b3b9eef29
alvyynm <alvyynm@gmail.com>
2022-10-25 15:06:07 +0300

    copy Footer.js contents to Social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,2 +1,21 @@
 import React from "react";
 import "./Social.scss";
+import slack from "../assets/slack.png";
+import github from "../assets/github.png";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="wrapper">
+        <a href="#" className="social-icon">
+          <img src={slack} alt="Slack logo" />
+        </a>
+        <a href="#" className="social-icon">
+          <img src={github} alt="GitHub logo" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
