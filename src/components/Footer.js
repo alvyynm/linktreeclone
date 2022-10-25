a39244fb0af1a09331a57b0ec05169e8f7521856
alvyynm <alvyynm@gmail.com>
2022-10-25 15:17:36 +0300

    delete Footer images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="wrapper">
-        <a href="#" className="social-icon">
-          <img src={slack} alt="Slack logo" />
-        </a>
-        <a href="#" className="social-icon">
-          <img src={github} alt="GitHub logo" />
-        </a>
-      </div>
+      <div className="wrapper"></div>
     </div>
   );
 };
