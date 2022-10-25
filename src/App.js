2cc37a2548c9e2d505b4c77650fd1fd237b80f69
alvyynm <alvyynm@gmail.com>
2022-10-25 15:13:59 +0300

    import Social.js and display inside app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Links from "./components/Links";
 import Profile from "./components/Profile";
 import Footer from "./components/Footer";
+import Social from "./components/Social";
 import "./App.css";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <div className="container">
       <Profile />
       <Links />
+      <Social />
       <Footer />
     </div>
   );
