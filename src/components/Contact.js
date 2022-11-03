c96e755d54e7b85edbcaa26026e614253a9c91f7
alvyynm <alvyynm@gmail.com>
2022-11-04 00:00:10 +0300

    setup: add JSX content to display form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,68 @@ import React from "react";
 import "./Contact.css";
 
 function Contact() {
-  return <div className="contact">Contact Us</div>;
+  return (
+    <main className="container">
+      <div className="form-container">
+        <header>
+          <h1>Contact Me</h1>
+          <p>Hi there, contact me to ask me about anything you have in mind.</p>
+        </header>
+        <form>
+          <fieldset>
+            <div class="row">
+              <div class="form-group">
+                <label for="first_name">First name </label>
+                <input
+                  type="text"
+                  name="first_name"
+                  id="first_name"
+                  placeholder="Enter your first name"
+                />
+              </div>
+              <div class="form-group">
+                <label for="last_name">Last name</label>
+                <input
+                  type="text"
+                  name="last_name"
+                  id="last_name"
+                  placeholder="Enter your last name"
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="yourname@email.com"
+              />
+            </div>
+
+            <div class="form-group">
+              <label for="message">Message</label>
+              <textarea
+                id="message"
+                placeholder="Send me a message and I'll reply as soon as possible..."
+              />
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" />
+              <label for="checkbox">
+                You agree to providing your data to Alvin who may contact you.
+              </label>
+            </div>
+          </fieldset>
+          <button type="submit" id="btn__submit">
+            Send message
+          </button>
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default Contact;
